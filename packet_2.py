9123bbce5409b0d34f1063bb1c54ff1e656e6ca4
signinshiva <busishiva123@gmail.com>
2024-01-09 21:49:02 +0530

    vohi h

---
+++ packet_2.py
@@ -0,0 +1,6 @@
+#length of the sentence
+
+text = "you can be anything  that you want to be with sheer determination hard work and consistancy"
+length = len(text)
+print("length of the string:",length)
+
